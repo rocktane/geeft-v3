dbd8e035726762f370ee0ae8034bdfeb478d9e71
Nina Laouadi <nina.laouadi0@gmail.com>
2024-08-15 13:54:44 +0200

    desactivation flatpickr mobile

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -5,15 +5,14 @@ import "flatpickr/dist/l10n/fr.js";
 export default class extends Controller {
   connect() {
 
-
-
-
     flatpickr(this.element, {
       altInput: true,
       altFormat: "d/m/Y",
       dateFormat: "Y-m-d",
       locale: "fr",
       allowInput: true,
+      disableMobile: true, // Ajoute cette ligne pour désactiver le sélecteur natif sur mobile
+
     });
   }
 }
