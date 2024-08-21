abe3f52b811263d2ce42996a47647c579608814e
Nina Laouadi <nina.laouadi0@gmail.com>
2024-08-21 15:06:38 +0200

    messages erreurs ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, uniqueness: { message: 'est déjà utilisé. Veuillez en choisir un autre.' }
+
+
   has_many :gifts, dependent: :destroy
   has_many :events, dependent: :destroy
 end
